a3e0a06984fe7b81f0d6398daf70d9e8ced56f15
Yihuai Gao <yihuai@stanford.edu>
2024-12-11 23:22:45 -0800

    Update record script for multiple objects

--- src/teleop_utils/mocap/scripts/load_mocap_trajectory.py
+++ src/teleop_utils/mocap/scripts/load_mocap_trajectory.py
@@ -7,15 +7,15 @@ import click
 @click.argument("data_file", type=click.Path(exists=True))
 def main(data_file: str):
     with open(data_file, "rb") as f:
-        data: list[tuple[npt.NDArray[np.float64], float]] = pickle.load(f)
+        data: dict[str, list[tuple[npt.NDArray[np.float64], float]]] = pickle.load(f)
     # print(len(data), data[0])
     print(len(data))
     if len(data) == 0:
         print("No data found")
         return
-    pose_xyz_wxyz: npt.NDArray[np.float64] = data[0][0]
-    mocap_timestamp: float = data[0][1]
-    print(pose_xyz_wxyz, mocap_timestamp)
+    for rigid_body_name, trajectory in data.items():
+        for pose_xyz_wxyz, mocap_timestamp in trajectory[:100]:
+            print(rigid_body_name, pose_xyz_wxyz, mocap_timestamp)
 
 
 if __name__ == "__main__":
--- src/teleop_utils/mocap/scripts/record_mocap_trajectory.py
+++ src/teleop_utils/mocap/scripts/record_mocap_trajectory.py
@@ -1,4 +1,5 @@
 
+from csv import DictWriter
 import numpy as np
 import time
 from ..mocap_client import MocapClient
@@ -10,12 +11,12 @@ import click
 
 
 @click.command()
-@click.argument("rigid_body_name", type=str)
-def main(rigid_body_name: str):
+@click.argument("rigid_body_names", type=str, nargs=-1)
+def main(rigid_body_names: list[str]):
     mocap_client = MocapClient()
     recording = False
     quit_flag = False
-    results: list[tuple[npt.NDArray[np.float64], float]] = []
+    results: dict[str, list[tuple[npt.NDArray[np.float64], float]]] = {rigid_body_name: [] for rigid_body_name in rigid_body_names}
     np.set_printoptions(precision=4, suppress=True, sign=" ")
 
     def on_press(key):
@@ -24,35 +25,36 @@ def main(rigid_body_name: str):
             nonlocal results
             recording = not recording
             if recording:
-                results = []
+                results = {rigid_body_name: [] for rigid_body_name in rigid_body_names}
                 print("Recording started")
             else:
                 current_file_dir = os.path.dirname(os.path.abspath(__file__))
-                data_file_name = f"{rigid_body_name}_{time.strftime('%Y-%m-%d_%H-%M-%S')}.pkl"
+                data_file_name = f"{'_'.join(rigid_body_names)}_{time.strftime('%Y-%m-%d_%H-%M-%S')}.pkl"
                 data_file_path = os.path.join(current_file_dir, "..", "data", data_file_name)
                 print(f"Recording stopped. Saving data to {data_file_path}")
                 with open(data_file_path, "wb") as f:
                     pickle.dump(results, f)
                 print("Data saved")
-                results = []
+                results = {rigid_body_name: [] for rigid_body_name in rigid_body_names}
         elif key == keyboard.Key.esc:
             nonlocal quit_flag
             quit_flag = True
 
     listener = keyboard.Listener(on_press=on_press)
     listener.start()
-    last_timestamp = 0
+    last_timestamp: dict[str, float] = {rigid_body_name: 0 for rigid_body_name in rigid_body_names}
     while not quit_flag:
         if recording:
-            result = mocap_client.get_latest_pose_xyz_wxyz(rigid_body_name)
-            if result is None:
-                continue
-            pose_xyz_wxyz, mocap_timestamp = result
-            if mocap_timestamp == last_timestamp:
-                continue
-            last_timestamp = mocap_timestamp
-            results.append((pose_xyz_wxyz, mocap_timestamp))
-            print(f"Timestamp: {mocap_timestamp:.6f}, Position: {pose_xyz_wxyz}")
+            for rigid_body_name in rigid_body_names:
+                result = mocap_client.get_latest_pose_xyz_wxyz(rigid_body_name)
+                if result is None:
+                    continue
+                pose_xyz_wxyz, mocap_timestamp = result
+                if mocap_timestamp == last_timestamp[rigid_body_name]:
+                    continue
+                last_timestamp[rigid_body_name] = mocap_timestamp
+                results[rigid_body_name].append((pose_xyz_wxyz, mocap_timestamp))
+                print(f"Object: {rigid_body_name}, Timestamp: {mocap_timestamp:.6f}, Position: {pose_xyz_wxyz}")
         time.sleep(1 / 120)
     
     listener.stop()
